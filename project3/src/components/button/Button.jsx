a0247706f60286a35512b800eb17ba101e93967f
veeresh8055 <visesync@gmail.com>
2026-01-18 12:02:05 +0530

    CSS MODULE

---
+++ project3/src/components/button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './Button.module.css'
+const Button = () => {
+  return (
+    <div className={styles.btn}>Button</div>
+  )
+}
+
+export default Button
---
+++ project3/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+        <h1>SHERYIENS </h1>
+        <p>CODING SCHOOL</p>
+    </div>
+  )
+}
+
+export default Header
