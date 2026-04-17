c8248463837aa0e6194d81f54d33224348a59dde
veeresh8055 <visesync@gmail.com>
2026-04-17 23:38:53 +0530

    Doc App

--- 19-DocApp/src/App.jsx
+++ 19-DocApp/src/App.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import Background from './components/Background'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='relative w-full h-screen bg-zinc-800'>
+       <Background />
+       <div className=' fixed left-0 z-3 w-full h-full  '>
+
+       </div>
+    </div>
   )
 }
 
