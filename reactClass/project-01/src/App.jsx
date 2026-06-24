ec5a0f679cb47466189b283c47f77e9788e14cdd
veeresh8055 <visesync@gmail.com>
2026-06-24 08:06:19 +0530

    project-01

---
+++ reactClass/project-01/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Welcome to React App.....</h1>
+  )
+}
+
+export default App
